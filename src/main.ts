4a450fa5b3172b6f6952a05a2ffb85353df361ca
Giowans <gio_tails@live.com>
2023-05-03 20:41:05 -0600

    extend body parser data limit.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.enableCors();
   const config = new DocumentBuilder()
   .addBearerAuth()
